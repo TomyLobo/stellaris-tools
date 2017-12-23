af93917c1246a5a68f2b3a5348f71eb8f4445141
TomyLobo <tomylobo@nurfuerspam.de>
2017-12-23 17:23:49 +0100

    Generate preprocessed variant of traits.json
    The raw version is now in traits_raw.json.
    Both versions are pretty_generate'd.

--- traitstojson.rb
+++ traitstojson.rb
@@ -8,6 +8,19 @@ require 'pp'
 
 config = JSON.parse(File.read('config.json'))
 
-save = parse_paradox_format(read_file_from_zip(find_mod_zip_by_id(config['mod_id']), config['traits_file']))
+save_raw = parse_paradox_format(read_file_from_zip(find_mod_zip_by_id(config['mod_id']), config['traits_file']))
+save = save_raw
+  .map { |trait_name, trait|
+    trait[0][:modifier][0]
+      .map{ |modifier, amount|
+        {
+          :name => trait_name,
+          :modifier => modifier,
+          :amount => amount[0],
+        }
+      }
+  }
+  .flatten(1)
 
-File.write('traits.json', JSON.dump(save))
+File.write('traits_raw.json', JSON.pretty_generate(save_raw))
+File.write('traits.json', JSON.pretty_generate(save))
